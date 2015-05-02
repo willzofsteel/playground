c034144681d258e1e269e99ce74376c8e82cfa91
William Andrews <william.andrews@gmail.com>
2015-05-02 09:00:26 -0700

    simple

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -1,24 +1,45 @@
 window.onload = function() {
 
-        var game = new Phaser.Game(
-          800,
-          600, 
-          Phaser.AUTO, 
-          '', 
-          { preload: preload, create: create }
-        );
+  var game = new Phaser.Game(
+      '99',
+      '99',
+      Phaser.AUTO,
+      '',
+      {
+        preload: preload,
+        create: create,
+        update: update
+      }
+      );
 
-        function preload () {
+  function preload () {
+    game.load.image('background', 'game_assets/m16.jpg');
+    game.load.spritesheet('ship', 'game_assets/spaceship.png', 32, 32, 4);
+  }
 
-            game.load.image('logo', 'game_assets/phaser.png');
+  function create () {
+    var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'background');
+    logo.anchor.setTo(0.5, 0.5);
 
-        }
+    var spaceship = game.add.sprite(game.world.centerX,
+                                    game.world.centerY,
+                                    'ship');
 
-        function create () {
+    spaceship.scale.x *= -1;
+    game.add.tween(spaceship).to( { angle: 180 }, 6000, Phaser.Easing.Linear.None, true);
+    spaceship.animations.add('walk');
+    spaceship.animations.play('walk', 1, true);
+    game.add.tween(spaceship).to(
+        { x:game.width },
+        9000,
+        Phaser.Easing.Linear.None, 
+        true,
+        2000);
 
-            var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-            logo.anchor.setTo(0.5, 0.5);
+//spaceship.scale.setTo(2,2);
+  }
 
-        }
+  function update () {
 
-    };
+  }
+};
