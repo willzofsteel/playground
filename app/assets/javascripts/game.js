2d12f2e33f53aa6fb5ad301d3b038ed247de8d70
William Andrews <william.andrews@gmail.com>
2015-05-04 22:13:29 -0700

    adding input

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -1,6 +1,19 @@
 window.onload = function() {
 
-  var game = new Phaser.Game(
+  (function () {
+    var leftKey     = null,
+        rightKey    = null,
+        downKey     = null,
+        upKey       = null,
+        posText     = null,
+        titleText   = null,
+        scoreText   = null,
+        score       = 0,
+        spaceship   = null;
+
+    
+
+    var game = new Phaser.Game(
       '99',
       '99',
       Phaser.AUTO,
@@ -12,34 +25,80 @@ window.onload = function() {
       }
       );
 
-  function preload () {
-    game.load.image('background', 'game_assets/m16.jpg');
-    game.load.spritesheet('ship', 'game_assets/spaceship.png', 32, 32, 4);
-  }
+    function preload () {
+      game.load.image('background', 'game_assets/m16.jpg');
+      game.load.spritesheet('ship', 'game_assets/spaceship.png', 32, 32, 4);
+    }
 
-  function create () {
-    var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'background');
-    logo.anchor.setTo(0.5, 0.5);
+    function create () {
+      var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'background');
+      logo.anchor.setTo(0.5, 0.5);
 
-    var spaceship = game.add.sprite(game.world.centerX,
-                                    game.world.centerY,
-                                    'ship');
+      titleText = game.add.text(600, 100, "Camille's Space Ace", {  font: '32px Arial', fill: '#ffffff' });
 
-    spaceship.scale.x *= -1;
-    game.add.tween(spaceship).to( { angle: 180 }, 6000, Phaser.Easing.Linear.None, true);
-    spaceship.animations.add('walk');
-    spaceship.animations.play('walk', 1, true);
-    game.add.tween(spaceship).to(
-        { x:game.width },
-        9000,
-        Phaser.Easing.Linear.None, 
-        true,
-        2000);
+      spaceship = game.add.sprite(game.world.centerX,
+          game.world.centerY,
+          'ship');
 
-//spaceship.scale.setTo(2,2);
-  }
+      spaceship.animations.add('walk');
+      posText = game.add.text(0, 0, "sprite position:" + spaceship.x + ',' + spaceship.y, {  font: '32px Arial', fill: '#ffffff' });
+      scoreText = game.add.text(1450, 0, "Score: " + score, { font: '24px Arial', fill: '#ffffff'});
 
-  function update () {
+      spaceship.scale.setTo(2,2);
+      game.camera.follow(spaceship);
+      game.physics.enable(spaceship, Phaser.Physics.ARCADE);
 
-  }
+      leftKey  = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+      rightKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+      upKey    = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+      downKey  = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+
+      game.input.keyboard.addKeyCapture([
+          Phaser.Keyboard.LEFT,
+          Phaser.Keyboard.RIGHT,
+          Phaser.Keyboard.UP,
+          Phaser.Keyboard.DOWN
+      ])
+
+      spaceship.body.collideWorldBounds = true;
+      spaceship.body.bounce.setTo(0.5, 0.5);
+      spaceship.body.velocity.x = 0;
+      spaceship.body.velocity.y = 0;
+    }
+
+    function update () {
+      if(leftKey.isDown){
+        updateShipHorizontal(-5);
+      }
+
+      if(rightKey.isDown){
+        updateShipHorizontal(5);
+      }
+
+      if(upKey.isDown){
+        updateShipVertical(-5);
+      }
+
+      if(downKey.isDown){
+        updateShipVertical(5);
+      }
+      posText.text = "position:" + Math.floor(spaceship.x) + ',' + Math.floor(spaceship.y);
+      posText.text += "| velocity:" + Math.floor(spaceship.body.velocity.x) + ',' + Math.floor(spaceship.body.velocity.y);
+      setScoreText();
+    }
+
+    function updateShipHorizontal(velocity) {
+        spaceship.body.velocity.x += velocity;
+        spaceship.animations.play('walk', 1, true);
+    }
+
+    function updateShipVertical(velocity) {
+        spaceship.body.velocity.y += velocity;
+        spaceship.animations.play('walk', 1, true);
+    }
+
+    function setScoreText() {
+      scoreText.text = "Score: " + score;
+    }
+  })();
 };
